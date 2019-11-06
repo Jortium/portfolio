a6bbbeef5541d11581ca489a6a4265b2aa74e3a9
OrtizJosh <joshwortiz@gmail.com>
2019-11-05 19:33:15 -0700

    Reupload of portfolio project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # portfolio
+
+My first protfolio website! Here you'll find more information on me the projects I have done as well as contact information to reach out to me. To build this webpage I used JQuery to make some transitions smoother. 
+
+My most challengeing hurdle of making this project had to be the CSS. Making the CSS just right so things function well and smoothly was a big hurdle.
+
+Feel free to reach out to me with comments concerns or feedback for this!
---
+++ docs/script.js
@@ -0,0 +1,107 @@
+function displayPortfolio(){
+    $('.splashPage').on('click', '.displayPortfolio',
+    function () {
+        navWelcome();       
+    });
+}
+
+function navWelcome(){
+    $('.splashContainer').remove();
+    $('.splashPage').fadeIn(1500).append(`<h1 class="welcome">Welcome</h1>`);
+    showPortfolio();
+ }
+
+
+function showPortfolio() {
+    $('.splashPage').delay(1000).fadeOut(1000);
+    showPage();
+    }
+
+function showPage(){
+    $('.pageContainer').delay(2000).fadeIn();
+    $('.pageHome').delay(3000).slideDown(1000);
+}
+
+function expand() {
+    $('.navMobile').on('click', '.navOpen',
+        function () {
+            showNav();
+        });
+}
+
+function contract() {
+    $('.navMobile').on('click', '.navClose',
+        function () {
+            hideNav();
+        });
+}
+
+function showNav() {
+    $('.navList').slideDown(800);
+    $('.navOpen').attr('src', 'images/closeNav.png').attr('class', 'navClose');
+}
+
+function hideNav() {
+    $('.navList').slideUp(800);
+    $('.navClose').attr('src', 'images/openNav.png').attr('class', 'navOpen');
+}
+
+function showHome() {
+    $('.displayBox').children().hide();
+    $('.pageHome').fadeIn(1000);
+    window.scrollTo(0, 0);
+    if ($(window).width() < 769) {
+        hideNav();
+    }
+}
+
+function showAbout() {
+    $('.displayBox').children().hide();
+    $('.pageAbout').fadeIn(1000);
+    window.scrollTo(0, 0);
+    if ($(window).width() < 769) {
+        hideNav();
+    }
+}
+
+function showProjects() {
+    $('.displayBox').children().hide();
+    $('.pageProjects').fadeIn(1000);
+    window.scrollTo(0, 0);
+    if ($(window).width() < 769) {
+        hideNav();
+    }
+}
+
+function navHome() {
+    $('.navWrap').on('click', '.navHome',
+        function () {
+            showHome();
+        })
+}
+
+function navAbout() {
+    $('.navWrap').on('click', '.navAbout',
+        function () {
+            showAbout();
+        })
+}
+
+function navProjects() {
+    
+    $('.navWrap').on('click', '.navProjects',
+        function () {
+            showProjects();
+        })
+}
+
+function handlePortfolio() {
+    displayPortfolio();
+    expand();
+    contract();
+    navHome();
+    navAbout();
+    navProjects();
+}
+
+handlePortfolio();
